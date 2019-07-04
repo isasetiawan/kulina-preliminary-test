e34c48e730ad7ffcd57b15fb382bb0f97f942474
isasetiawan <azaqokay@gmail.com>
2019-07-05 04:27:50 +0700

    Create rating.go
    raing.go : create a basic model for user_review

---
+++ basic_coding/model/rating.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type UserReview struct {
+	gorm.Model
+	OrderId   int    `json:"order_id" validate:"required"`
+	ProductId int    `json:"product_id" validate:"required"`
+	UserId    int    `json:"user_id" validate:"required"`
+	Rating    int    `json:"rating" validate:"required"`
+	Review    string `json:"review"`
+}
