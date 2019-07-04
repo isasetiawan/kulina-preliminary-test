2d874411286847efc403c7457d328e5e0df8d1f8
isasetiawan <azaqokay@gmail.com>
2019-07-05 04:54:42 +0700

    rating.go : add rating range validator

--- basic_coding/model/rating.go
+++ basic_coding/model/rating.go
@@ -9,6 +9,6 @@ type UserReview struct {
 	OrderId   int    `json:"order_id" validate:"required"`
 	ProductId int    `json:"product_id" validate:"required"`
 	UserId    int    `json:"user_id" validate:"required"`
-	Rating    int    `json:"rating" validate:"required"`
+	Rating    int    `json:"rating" validate:"required,gte=1,lte=5"`
 	Review    string `json:"review"`
 }
